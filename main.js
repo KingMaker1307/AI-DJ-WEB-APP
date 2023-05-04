63213f98ed8daa988734e465fb3d9fae7bf0f26f
KingMaker1307 <121443296+KingMaker1307@users.noreply.github.com>
2023-05-04 19:35:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+
+song="";
 function setup() {
     canvas = createCanvas(500,500);
-    canvas.center();
+    canvas.position(430,200);
+    video = createCapture(VIDEO);
+    video.hide(); 
+}
+function preload() {
+    song = loadSound("music.mp3");
+    song = loadSound("music2.mp3");
+}
+function draw() {
+    image(video,0,0,500,500);
 }
